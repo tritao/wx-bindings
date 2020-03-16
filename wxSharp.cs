743b19b6d33e9c349afbeb31313be100d48a7050
João Matos <joao@tritao.eu>
2020-03-16 14:59:41 +0000

    Fix indentation to spaces.

--- wxSharp.cs
+++ wxSharp.cs
@@ -12,20 +12,20 @@ namespace CppSharp
             var options = driver.Options;
             options.LibraryName = "wxSharp";
             //options.Headers.Add("wx/list.h");
-			options.Headers.Add("wx/app.h");
-			var wxSharp = GetExamplesDirectory ("wxSharp");
-			var wxPath = Path.Combine (wxSharp, "wxWidgets/include");
-			options.addIncludeDirs(wxPath);
-			options.addDefines("HAVE_SSIZE_T");
-			options.addDefines("_FILE_OFFSET_BITS=64");
-			options.addDefines("WXUSINGDLL");
-			options.addDefines("__WXMAC__");
-			options.addDefines("__WXOSX__");
-			options.addDefines("__WXOSX_COCOA__");
-			options.TargetTriple = "i686-apple-darwin12.4.0";
-			options.Verbose = true;
+            options.Headers.Add("wx/app.h");
+            var wxSharp = GetExamplesDirectory ("wxSharp");
+            var wxPath = Path.Combine (wxSharp, "wxWidgets/include");
+            options.addIncludeDirs(wxPath);
+            options.addDefines("HAVE_SSIZE_T");
+            options.addDefines("_FILE_OFFSET_BITS=64");
+            options.addDefines("WXUSINGDLL");
+            options.addDefines("__WXMAC__");
+            options.addDefines("__WXOSX__");
+            options.addDefines("__WXOSX_COCOA__");
+            options.TargetTriple = "i686-apple-darwin12.4.0";
+            options.Verbose = true;
 
-			options.OutputDir = Path.Combine(wxSharp, options.TargetTriple);
+            options.OutputDir = Path.Combine(wxSharp, options.TargetTriple);
 
         }
 
@@ -41,29 +41,29 @@ namespace CppSharp
         {
         }
 
-		public static string GetExamplesDirectory(string name)
-		{
-			var directory = Directory.GetParent(Directory.GetCurrentDirectory());
+        public static string GetExamplesDirectory(string name)
+        {
+            var directory = Directory.GetParent(Directory.GetCurrentDirectory());
 
-			while (directory != null)
-			{
-				var path = Path.Combine(directory.FullName, "examples", name);
+            while (directory != null)
+            {
+                var path = Path.Combine(directory.FullName, "examples", name);
 
-				if (Directory.Exists(path))
-					return path;
+                if (Directory.Exists(path))
+                    return path;
 
-				directory = directory.Parent;
-			}
+                directory = directory.Parent;
+            }
 
-			throw new Exception(string.Format(
-				"Examples directory for project '{0}' was not found", name));
-		}
+            throw new Exception(string.Format(
+                "Examples directory for project '{0}' was not found", name));
+        }
 
         static class Program
         {
             public static void Main(string[] args)
             {
-				var dir = Directory.GetCurrentDirectory ();
+                var dir = Directory.GetCurrentDirectory ();
 
                 ConsoleDriver.Run(new wxSharpGen());
             }
